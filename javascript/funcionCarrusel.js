33341f251d52d19fc7d8acd64fcd32f4cf6f3cec
Javier <xavier11.jd@gmail.com>
2018-09-20 17:28:00 -0500

    Iniciamos

---
+++ javascript/funcionCarrusel.js
@@ -0,0 +1,18 @@
+  $(function() 
+        {            
+        $(function() 
+        {           
+            $('#carrusel').carouFredSel({
+                    width: '100%',
+                    items: {
+                        visible: 3,
+                        start: -1
+                    },
+                    scroll: {
+                        items: 1,
+                        duration: 1000
+                    }
+                    }
+                );
+        })
+        })
